ee380195cdf10ba9e9ae2eec1e432b609c460187
Litty Mathew <littymathew3008@gmail.com>
2022-09-30 14:11:06 +0100

    updated documentation 2

---
+++ R/studioalbumtracksdoc.R
@@ -0,0 +1,11 @@
+#' studio album data
+#'
+#'got from github
+#'
+#' @format A dataframe with 31 rows and 25 variables:
+#' \describe{
+#'   \item{Site}{Site ID number}
+#'   \item{Day}{Day of data collected}
+#'   }
+#' @source <https://besjournals.onlinelibrary.wiley.com/doi/full/10.1111/j.1365-2745.2007.01225.x>
+"studio_album_tracks"
